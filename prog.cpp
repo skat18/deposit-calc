1f9f3cf313e61d619677d5a340207c49b3a91bcc
Сорокина Александра Сергеевна <ip614s17@mwp07.csc.local>
2017-02-22 14:17:35 +0700

    prog.cpp

--- prog.cpp
+++ prog.cpp
@@ -1,23 +1,36 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <iostream>
+#include <stdio.h>
+#include <ctime>
+#include <stdlib.h>
+
 using namespace std;
+
 int main() {
-
-int cash=0, srok=0,vklad=0 ;
-
-cout<<"Vvedite cash"<<endl;
+int cash=0, srok=0;
+cout<<"Enter the amount"<<endl;
 cin>>cash;
-cout<<"Vvedite srok"<<endl;
+if(cash<10000) {
+cout<<"Error"<<endl;
+return 0;
+}
+cout<<"Enter srok vklada"<<endl;
 cin>>srok;
-
-if(srok<365 && srok!=0){
-    if(srok<=30){
-    vklad=cash-cash*0.1;
-    }
-    else if(srok>=31 && srok<=120){
-    vklad=cash+cash*0.02;
+if(srok>365 || srok<0) {
+cout<<"Error"<<endl;
+return 0;
+}
+if(cash<=100000 && cash>=10000) {
+    if(srok>0 && srok<=30) {
+      cash=cash-(cash*0.1);
+    } else if(srok>=31 && srok<=120) {
+        cash=cash+(cash*0.02);
+    } else if(srok>=121 && srok<=240) {
+        cash=cash+(cash*0.06);
+    } else if(srok>=241 && srok<=365) {
+        cash=cash+(cash*0.12);
     }
 }
+cout<<cash<<endl;
+system("pause");
 return 0;
 }
