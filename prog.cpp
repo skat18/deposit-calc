e2f00236929afcf19e23719b92c463c6f8dd1830
Сорокина Александра Сергеевна <ip614s17@mwp07.csc.local>
2017-02-22 14:53:42 +0700

    state 4

--- prog.cpp
+++ prog.cpp
@@ -30,10 +30,15 @@ if(cash<=100000 && cash>=10000) {
         cash=cash+(cash*0.12);
     }
 } else if (cash>100000) {
-	if (srok>0 && srok<=30)
+    if (srok>0 && srok<=30)
 	cash=cash-(cash*0.1);}
-	else if(srok>=31 && srok<=120) {
+    else if(srok>=31 && srok<=120) {
         cash=cash+(cash*0.03);}
+    else if(srok>=121 && srok<=240) {
+	cash=cash+(cash*0.08);}
+    else if(srok>=241 && srok<=365) {
+	cash=cash=(cash*0.15);}
+}
 cout<<cash<<endl;
 system("pause");
 return 0;
